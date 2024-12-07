8e6e2af1a91872a775a63502eaa1ead7d8e23d92
Rizen <123223112+royalrizen@users.noreply.github.com>
2024-12-07 20:59:59 +0530

    Create terraria.py

---
+++ commands/terraria.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+from utils.terraria_server_renew import renew_terraria_server
+
+class Terraria(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="renew", help="Renews Terraria server")
+    async def _renew(self, ctx, url: str):
+        try:
+            await renew_terraria_server(url)
+            await ctx.send(f"Successfully renewed Terraria server on: **{url}**")
+        except Exception as e:
+            await ctx.send(e)
+
+async def setup(bot):
+    await bot.add_cog(Terraria(bot))
